638d6ffb62ab12cefc4e75aa5ffb6d13b65773c9
Mohammad Smadi <mo.smadi@outlook.com>
2022-02-22 15:51:32 +0200

    convert array to list

--- StackTask/MyStack.cs
+++ StackTask/MyStack.cs
@@ -2,15 +2,9 @@ namespace StackTask;
 
 public class MyStack <T>
 {
-    private readonly T[] _items;
-    private int _index = -1;
-    public MyStack()// =>   _items = new T[10];
+    private readonly List<T> _items;
+    public MyStack(List<T> items)// =>   _items = new T[10];
     {
-        _items = new T[10];
-    }
-    
-    public MyStack(int size)// =>   _items = new T[size];
-    {
-        _items = new T[size];
+        _items = items;
     }
 }
